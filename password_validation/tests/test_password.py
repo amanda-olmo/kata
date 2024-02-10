167c9d2f6141450e8fa5151c2192d123839c234b
amanda-olmo <amanda.olmo.garcia@gmail.com>
2024-02-10 13:44:29 +0000

    [Password kata] Conditions for good password

--- password_validation/tests/test_password.py
+++ password_validation/tests/test_password.py
@@ -1,7 +1,20 @@
 # Python libraries
 import pytest
 # Own libraries
+from src.main import isValid
+
+"""
+Conditions
+- Minimum 8 characters
+- At least one upper case letter
+- At least on lower case letter
+- At least one number
+- At least one underscore
+
+"""
 
 class TestPassword:
-    def test_prueba(self):
-        assert 0 == 0
+
+    def test_good_password(self):
+        password = 'aA1_aaaa'
+        assert isValid(password) is True
