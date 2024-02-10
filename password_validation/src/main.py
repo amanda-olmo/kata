a68ab613bb1a6f9e8e29b2e44e8da391df07f6b3
amanda-olmo <amanda.olmo.garcia@gmail.com>
2024-02-10 15:05:44 +0000

    [Password kata] Test underscore - Green

--- password_validation/src/main.py
+++ password_validation/src/main.py
@@ -12,4 +12,5 @@ def isValid(password):
     has_upper_case = any([letter.isupper() for letter in password])
     has_lower_case = any([letter.islower() for letter in password])
     has_number = any([letter.isdigit() for letter in password])
-    return has_minimal_length and has_upper_case and has_lower_case and has_number
+    has_underscore = '_' in password
+    return has_minimal_length and has_upper_case and has_lower_case and has_number and has_underscore
