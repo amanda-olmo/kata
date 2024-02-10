598b63ce9ba761625cc0a7e65026c07bdf0ccc3b
amanda-olmo <amanda.olmo.garcia@gmail.com>
2024-02-10 14:10:16 +0000

    [Password kata] Test lower case letter - Green

--- password_validation/src/main.py
+++ password_validation/src/main.py
@@ -10,4 +10,5 @@ Conditions
 def isValid(password):
     has_minimal_length = len(password) >= 8
     has_upper_case = any([letter.isupper() for letter in password])
-    return has_minimal_length and has_upper_case
+    has_lower_case = any([letter.islower() for letter in password])
+    return has_minimal_length and has_upper_case and has_lower_case
