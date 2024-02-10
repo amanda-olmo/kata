5165c6197a428c892a89ae6d75b952d7375a7f4b
amanda-olmo <amanda.olmo.garcia@gmail.com>
2024-02-10 13:46:21 +0000

    [Password kata] Create password validation function

---
+++ password_validation/src/main.py
@@ -0,0 +1,11 @@
+"""
+Conditions
+- Minimum 8 characters
+- At least one upper case letter
+- At least on lower case letter
+- At least one number
+- At least one underscore
+"""
+
+def isValid(password):
+    return True
